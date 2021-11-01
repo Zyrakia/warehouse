17a71f7d5821541ce2f2f4d9e48929e12e62293f
Zyrakia <mailzyrakia@gmail.com>
2021-11-01 16:45:52 -0700

    fix: exported new guards and transformers

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,14 @@ export { Guard, GuardUtils } from './guard/Guard';
 export { Transformer, TransformerUtils } from './transformer/Transformer';
 export { UpdateSource } from './types/UpdateSource';
 export { SortOrder } from './types/SortOrder';
+
+// Guards
+export { ClampGuard } from './guard/default-guards/ClampGuard';
+export { GamepassGuard } from './guard/default-guards/GamepassGuard';
+export { PremiumGuard } from './guard/default-guards/PremiumGuard';
+export { ReadonlyGuard } from './guard/default-guards/ReadonlyGuard';
+export { ServerGuard } from './guard/default-guards/ServerGuard';
+export { TypeGuard } from './guard/default-guards/TypeGuard';
+
+// Transformers
+export { IncrementTransformer } from './transformer/default-transformers/IncrementTransformer';
