a351c05bde8879e3aeea1ff78d2717c0bb68bcc4
Zyrakia <mailzyrakia@gmail.com>
2021-11-01 15:22:49 -0700

    build: exported necessary objects in index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export { Warehouse } from './warehouse/Warehouse';
+export { OrderedWarehouse } from './warehouse/OrderedWarehouse';
+export { WarehouseHandlers, OrderedWarehouseHandlers } from './types/WarehouseHandlers';
+export { Guard, GuardUtils } from './guard/Guard';
+export { Transformer, TransformerUtils } from './transformer/Transformer';
 export { UpdateSource } from './types/UpdateSource';
+export { SortOrder } from './types/SortOrder';
