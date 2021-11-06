9c51081bb8104491b6b44685f63115f9ff3152e9
Zyrakia <mailzyrakia@gmail.com>
2021-11-05 21:01:52 -0700

    refactor: rename tests folder

--- src/test-runner.server.ts
+++ src/test-runner.server.ts
@@ -2,6 +2,6 @@ import { ReplicatedStorage } from '@rbxts/services';
 import Test from '@rbxts/testez';
 
 Test.TestBootstrap.run(
-	[ReplicatedStorage.WaitForChild('out').WaitForChild('tests')],
+	[ReplicatedStorage.WaitForChild('out').WaitForChild('__tests__')],
 	Test.Reporters.TextReporterQuiet,
 );
