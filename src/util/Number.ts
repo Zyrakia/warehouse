c9f8a36f37b01ce142601adfbec2c736b2ec2c36
Zyrakia <mailzyrakia@gmail.com>
2021-11-28 15:34:46 -0800

    fix: removed n for bigint reference

--- src/util/Number.ts
+++ src/util/Number.ts
@@ -1,5 +1,5 @@
 export namespace Number {
-	export const MAX_64BIT_INT = 9223372036854775807n;
+	export const MAX_64BIT_INT = 9223372036854775807;
 	export const MAX_SAFE_CHARACTERS = 4000000;
 	export const MAX_SAFE_KEY_CAHRACTERS = 50;
 }
